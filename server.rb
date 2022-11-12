dd5fbb3717c19bfafb692902bbd8e898fb852b12
Simon Addicott <simonaddicott@Simons-MacBook-Pro.local>
2022-11-12 22:27:54 +0000

    Change repsponse payload

--- server.rb
+++ server.rb
@@ -2,6 +2,6 @@ require 'socket'
 socket = TCPServer.new('0.0.0.0', 8080)
 while(client = socket.accept)
 	puts "New client! #{client}"
-	client.write("Hello Aaron")
+	client.write("Hello Client!")
 	client.close
 end
